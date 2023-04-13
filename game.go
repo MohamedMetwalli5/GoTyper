53cbf9853395a0bc428091da12bb0845691d25c9
MohamedMetwalli5 <mohamedmetwalli5@gmail.com>
2023-04-13 23:14:45 +0200

    Adding Login & SignUp Options!

--- game.go
+++ game.go
@@ -121,7 +121,7 @@ func MetricsCalculation(username string, password string, input string, text str
 	raw := int(float32(len(input)) / (float32(elapsed * 60)))
 	fmt.Printf("\033[33mRaw: %d\033[0m\n", raw)
 
-	insertInDataBase(username, password, strconv.Itoa(wpm), strconv.Itoa(acc), strconv.Itoa(raw))
+	updateDataBase(username, password, strconv.Itoa(wpm), strconv.Itoa(acc), strconv.Itoa(raw))
 }
 
 func clear() {
@@ -136,14 +136,51 @@ func CheckError(err error) {
 	}
 }
 
-func insertInDataBase(username string, password string, wpm string, acc string, raw string) {
+func updateDataBase(username string, password string, wpm string, acc string, raw string) {
+	psqlconn := fmt.Sprintf("host= %s port= %d user= %s password= %s dbname= %s sslmode=disable", host, port, user, admin_password, dbname)
+	db, err := sql.Open("postgres", psqlconn)
+	CheckError(err)
+	defer db.Close()
+
+	updateStatement := `UPDATE users
+						SET username = $1, password = $2, wpm = $3, acc = $4, raw = $5 
+						WHERE username = $1;
+						`
+	_, e := db.Exec(updateStatement, username, password, wpm, acc, raw)
+	CheckError(e)
+}
+
+func Login(username string, password string) bool {
+	psqlconn := fmt.Sprintf("host= %s port= %d user= %s password= %s dbname= %s sslmode=disable", host, port, user, admin_password, dbname)
+	db, err := sql.Open("postgres", psqlconn)
+	CheckError(err)
+	defer db.Close()
+
+	checkStatement := `SELECT username, password, wpm, acc, raw
+					   FROM users
+					   where username = $1 and password = $2;
+					  `
+	row := db.QueryRow(checkStatement, username, password)
+	switch err := row.Scan(&username, &password); err {
+	case sql.ErrNoRows:
+		fmt.Println("Wrong username or password. Try again!")
+		return false
+	case nil:
+		return true
+	default:
+		fmt.Println("Welcome back,", username+"!")
+		return true
+	}
+}
+
+func SignUp(username string, password string) {
 	psqlconn := fmt.Sprintf("host= %s port= %d user= %s password= %s dbname= %s sslmode=disable", host, port, user, admin_password, dbname)
 	db, err := sql.Open("postgres", psqlconn)
 	CheckError(err)
 	defer db.Close()
 
 	insertDynamicStmt := `insert into "users" ("username", "password", "wpm", "acc", "raw") values ($1, $2, $3, $4, $5)`
-	_, e := db.Exec(insertDynamicStmt, username, password, wpm, acc, raw)
+	_, e := db.Exec(insertDynamicStmt, username, password, 0, 0, 0)
 	CheckError(e)
 }
 
@@ -167,17 +204,38 @@ func main() {
 	clear()
 	println()
 
-	fmt.Print("Enter Username : ")
 	username := ""
-	fmt.Scanln(&username)
-
-	fmt.Print("Enter Password : ")
 	password := ""
-	fmt.Scanln(&password)
 
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Println("(1) SignUp\n(2) Login")
+	passingOption := ""
+	if scanner.Scan() {
+		passingOption = scanner.Text()
+		if passingOption == "1" {
+			fmt.Print("Enter Username : ")
+			fmt.Scanln(&username)
+
+			fmt.Print("Enter Password : ")
+			fmt.Scanln(&password)
+			SignUp(username, password)
+		} else {
+			loginFlag := false
+			for !loginFlag {
+				fmt.Print("Enter Username : ")
+				fmt.Scanln(&username)
+
+				fmt.Print("Enter Password : ")
+				fmt.Scanln(&password)
+
+				loginFlag = Login(username, password)
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}
 	clear()
 	println()
-	scanner := bufio.NewScanner(os.Stdin)
 
 	fmt.Println("Choose a level : \n(1) Easy\n(2) Medium\n(3) Hard")
 	level := ""
